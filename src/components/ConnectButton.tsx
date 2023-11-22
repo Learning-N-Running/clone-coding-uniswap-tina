dcbf2fcfc08ad1df0622bf0ffc0cc00179432c77
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-11-22 15:49:36 +0900

    Fix: styles

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -4,10 +4,13 @@ export default function ConnectButton() {
   const { isConnected } = useAccount();
   console.log("isConnected", isConnected);
   return (
-    !isConnected && (
-      <div>
-        <w3m-button />
-      </div>
-    )
+    // !isConnected && (
+    //   <div>
+    //     <w3m-button />
+    //   </div>
+    // )
+    <div style={{ marginBottom: "10px" }}>
+      <w3m-button />
+    </div>
   );
 }
